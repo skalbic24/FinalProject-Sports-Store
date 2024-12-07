45d28a161663b2c21651d9ed1da7c6793a0ec24f
Sanel Kalbic <sanelkalbic@Sanels-MacBook-Pro.local>
2024-12-07 15:48:19 -0600

    note udpates

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,11 @@
+/*
+Using an online API of your choice, create a React project. You will be working on this for the next three weeks.
+Project must meet the following criteria:
+Use React Router and have at least 3 pages using React Bootstrap or an alternative styling library
+Contain at least 10 custom components
+Allow for all CRUD operations via one or more APIs */
+
+
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
